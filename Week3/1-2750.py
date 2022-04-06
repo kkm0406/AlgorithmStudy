bb719d7ead3f39925e2fa995e5f3442373d54829
Kim Kwangmo <sdjmc30412@naver.com>
2022-04-07 00:37:48 +0900

    정렬 2750 수 정렬하기

--- Week3/1-2750.py
+++ Week3/1-2750.py
@@ -2,8 +2,13 @@ n = int(input())
 arr = []
 for i in range(n):
     arr.append((int(input())))
+num = 0
+for i in range(0, len(arr)-1):
+    num = i
+    for j in range(i + 1, len(arr)):
+        print(num, arr[j])
+        if arr[j] < arr[num]:
+            arr[num], arr[j] = arr[j], arr[num]
 
-newArr = sorted(arr)
-
-for i in newArr:
+for i in arr:
     print(i)
