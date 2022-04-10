db93cbb9bb4e61439043fac91da4dbf7eea137b5
Kim Kwangmo <sdjmc30412@naver.com>
2022-04-10 11:13:40 +0900

    정렬 10989 수 정렬하기3

---
+++ Week3/3-10989.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+num_list = [0] * 10001 #리스트를 0으로 초기화
+
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1
+    #입력값을 받을 때마다 입력값과 같은 인덱스에 +1
+
+for i in range(10001):
+    if num_list[i] != 0: #0보다 큰 요소
+        for j in range(num_list[i]):  #해당 인덱스들을 찾아 그 수만큼 출력
+            print(i)
+
+#1. 리스트를 0으로 초기화
+#2. 입력값을 받을 때 입력값과 같은 인덱스에 1씩 더함
+#3. 리스트를 돌면서 0보다 큰 요소를 갖는 인덱스가 있으면
+#   인덱스에 저장된 수만큼 인덱스를 출력
