9574da27cf77cd1047acb563338e253467a32a10
Kim Kwangmo <sdjmc30412@naver.com>
2022-04-10 11:24:17 +0900

    정렬 11651 좌표 정렬하기2

--- Week3/7-11651.py
+++ Week3/7-11651.py
@@ -6,7 +6,7 @@ for i in range(n):
     a, b = map(int, sys.stdin.readline().split())
     arr.append((b, a))
 
-arr.sort()
+arr.sort() #sort할 때 첫번째 요소가 같으면 두번째 요소 비교
 for i in range(len(arr)):
     print(arr[i][1], arr[i][0])
 
