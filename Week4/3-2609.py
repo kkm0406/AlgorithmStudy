1b24071db9fcdb205e6801f6c7aa7c6cb66dd47e
Kim Kwangmo <sdjmc30412@naver.com>
2022-04-23 13:22:54 +0900

    정수론 및 조합론 2609 최대송약수와 최소공배수

---
+++ Week4/3-2609.py
@@ -0,0 +1,15 @@
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+
+numA = 0
+for i in range(1, min(a, b) + 1):
+    if a % i == 0 and b % i == 0:
+        numA = i
+
+print(numA)
+
+for i in range(max(a, b), a * b + 1):
+    if i % a == 0 and i % b == 0:
+        print(i)
+        break
