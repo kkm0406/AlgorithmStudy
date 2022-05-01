914eb32f1d3cec4552e964388bd9c23ea92c6133
kkm0406 <sdjmc30412@naver.com>
2022-05-01 12:52:23 +0900

    정수론 및 조합론 1934 최소공배수

---
+++ Week4/4-1934.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+
+def find(n1, n2):
+    if n2 == 0:
+        return n1
+    else:
+        return find(n2, n1 % n2)
+
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    n1 = max(a, b)
+    n2 = min(a, b)
+    print(int(n1*n2/find(n1, n2)))
