a246b7e176461b19acbc3ae725809313ffb8f0db
kkm0406 <sdjmc30412@naver.com>
2022-05-02 12:01:01 +0900

    정수론 및 조합론 11050 이항 계수 1

--- Week4/6-3036.py
+++ Week4/6-3036.py
@@ -1,19 +1,13 @@
 import sys
 
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-num = arr[0]
+a, b = map(int, sys.stdin.readline().split())
 
 
-def find(a, b):
-    if b == 0:
-        return a
-    else:
-        return find(b, a % b)
+def fact(n):
+    num = 1
+    for i in range(2, n+1):
+        num *= i
+    return num
 
 
-for i in range(1, len(arr)):
-    n1 = max(num, arr[i])
-    n2 = min(num, arr[i])
-    gcd = find(n1, n2)
-    print(str(int(num / gcd)) + '/' + str(int(arr[i] / gcd)))
+print(int(fact(a) / (fact(b) * fact(a - b))))
