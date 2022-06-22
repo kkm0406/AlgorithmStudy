79b265921112d4c93dbd9fdddedba3af90d089fc
kkm0406 <sdjmc30412@naver.com>
2022-06-23 03:37:46 +0900

    백트래킹 1932 정수 삼각형

---
+++ Week6/7-1932.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, n):
+    for j in range(len(arr[i])):
+        if j == 0:
+            arr[i][j] += + arr[i - 1][j]
+        elif j == len(arr[i]) - 1:
+            arr[i][j] += arr[i - 1][j - 1]
+        else:
+            arr[i][j] += max(arr[i - 1][j - 1], arr[i - 1][j])
+print(max(arr[n - 1]))
