7d35dfc70bea47612684460ab7ad82732b4a3fdc
kkm0406 <sdjmc30412@naver.com>
2022-06-27 12:45:38 +0900

    백트래킹 11054 가장 긴 바이토닉 부분 수열

---
+++ Week6/13-11054.py
@@ -0,0 +1,30 @@
+N = int(input())
+
+List = list(map(int, input().split()))
+
+dp1 = [1] * N
+dp2 = [1] * N
+
+sub_len = [0] * N
+
+Max = 0
+
+for i in range(N):
+    for j in range(i):
+        if List[i] > List[j]:
+            dp1[i] = max(dp1[i], dp1[j] + 1)
+
+List.reverse()
+
+for i in range(N):
+    for j in range(i):
+        if List[i] > List[j]:
+            dp2[i] = max(dp2[i], dp2[j] + 1)
+dp2.reverse()
+
+for i in range(N):
+    sub_len[i] = dp1[i] + dp2[i]
+
+print(max(sub_len) - 1)
+
+#증가하는 부분수열 구하고, 감소하는 부분수열 구하고, 둘을 더함
