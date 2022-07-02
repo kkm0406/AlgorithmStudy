7c5d0282f0905127145ec7322f9b2b68e84e0df0
kkm0406 <sdjmc30412@naver.com>
2022-07-02 17:33:29 +0900

    동적 계획법 2579 계단 오르기

--- Week6/8-2579.py
+++ Week6/8-2579.py
@@ -1,18 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = []
-for _ in range(n):
-    arr.append(int(sys.stdin.readline()))
-dp = [0]*n
+arr = [0 for i in range(301)]
+for i in range(n):
+    arr[i] = int(sys.stdin.readline())
+dp = [0 for i in range(301)]
 
 dp[0] = arr[0]
-dp[1] = arr[0]+arr[1]
-dp[2] = max(arr[0]+arr[1], arr[0]+arr[2])
+dp[1] = arr[0] + arr[1]
+dp[2] = max(arr[1] + arr[2], arr[0] + arr[2])
 for i in range(3, n):
-    dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
+    dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i])
 
-print(dp[n-1])
+print(dp[n - 1])
 
 # dp리스트의 첫번째에는 당연히 s리스트 첫번째 점수가 들어가고,
 #
