43fc87df65ce9b4807d252fe9903a151a579941b
kkm0406 <sdjmc30412@naver.com>
2022-07-28 16:43:02 +0900

    큐, 덱 18258 큐 2

---
+++ 큐, 덱/1-18258.py
@@ -0,0 +1,32 @@
+import sys
+
+n = int(sys.stdin.readline())
+q = []
+for _ in range(n):
+    op = sys.stdin.readline().split()
+    if op[0] == "push":
+        q.append(op[1])
+    elif op[0] == "front":
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif op[0] == "back":
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
+    elif op[0] == "size":
+        print(len(q))
+    elif op[0] == "empty":
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif op[0] == "pop":
+        if len(q) == 0:
+            print(-1)
+        else:
+            q.reverse()
+            print(q.pop())
+            q.reverse()
