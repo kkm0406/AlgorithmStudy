078a3e7cf23c4579206e3533ec7c672d88c69721
kkm0406 <sdjmc30412@naver.com>
2022-07-28 20:09:16 +0900

    큐, 덱 2164 카드2

--- 큐, 덱/2-2164.py
+++ 큐, 덱/2-2164.py
@@ -1,4 +1,15 @@
 import sys
+from collections import deque
 
 n = int(sys.stdin.readline())
-q = []
+q = deque()
+
+for i in range(1, n + 1):
+    q.append(i)
+
+while len(q) > 1:
+    q.popleft()
+    front = q.popleft()
+    q.append(front)
+
+print(q[0])
