c938b667a01d3276228ba146661d3f2c40f96161
kkm0406 <sdjmc30412@naver.com>
2022-09-24 21:30:01 +0900

    백준 알고리즘(2) 11728 배열합치기

---
+++ 알고리즘(2)/11728.py
@@ -0,0 +1,8 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
+c = sorted(a+b)
+for i in c:
+    print(i, end=" ")
