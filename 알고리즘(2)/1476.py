21e0a51727e15dc0aa4a88fc49b7f610cc9a147c
kkm0406 <sdjmc30412@naver.com>
2022-09-25 16:50:37 +0900

    백준 알고리즘(2) 1476 날짜 계산

---
+++ 알고리즘(2)/1476.py
@@ -0,0 +1,9 @@
+import sys
+
+e, s, m = map(int, sys.stdin.readline().split())
+year = 0
+while True:
+    if (year - e) % 15 == 0 and (year - s) % 28 == 0 and (year - m) % 19 == 0:
+        print(year)
+        break
+    year += 1
