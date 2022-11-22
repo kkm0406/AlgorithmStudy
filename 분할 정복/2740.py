bd3c6afad856b8b0551e023d37abd3ca58623dbd
kkm0406 <sdjmc30412@naver.com>
2022-11-22 23:50:36 +0900

    백준 분할정복 2740 행렬 곱셈

---
+++ 분할 정복/2740.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+A = []
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+
+M, K = map(int, input().split())
+B = []
+for _ in range(M):
+    B.append(list(map(int, input().split())))
+
+
+C = [[0 for _ in range(K)] for _ in range(N)]
+
+for n in range(N):
+    for k in range(K):
+        for m in range(M):
+            C[n][k] += A[n][m] * B[m][k]
+
+for i in range(len(C)):
+    for j in C[i]:
+        print(j, end=' ')
+    print()
+
